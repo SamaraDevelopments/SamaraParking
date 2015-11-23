c639a41d8e8aacd84b362f8ba1e5a17218933674
Mauricio <maurisho01@gmail.com>
2015-11-23 13:20:41 -0600

    Active User
    add methos and ActiveUser.aspx

---
+++ App_Code/Business/Activeuser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+/// <summary>
+/// Summary description for ActiveUser
+/// </summary>
+public class ActiveUser
+{
+    public ActiveUser()
+    {
+
+    }
+    public DataTable GetDataTableOfActiveProfessor()
+    {
+        UserData usd = new UserData();
+        return usd.GetActiveProfessor();
+    }
+    public DataTable GetDataTableOfActivestudent()
+    {
+        UserData usd = new UserData();
+        return usd.GetActiveStudent();
+    }
+}
